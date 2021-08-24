82a1605b578c0e0a42dfde5715522beb22b41039
Alessandro <none>
2021-08-24 16:32:44 +0200

    added node_count. version bump

--- src/utils.jl
+++ src/utils.jl
@@ -6,3 +6,10 @@ the argument to the predicate satisfies `isterm` and `gethead(x) == f`
 """
 is_operation(f) = @nospecialize(x) -> isterm(x) && (gethead(x) == f)
 export is_operation
+
+
+"""
+    node_count(t)
+Count the nodes in a symbolic expression tree satisfying `isterm` and `getargs`.
+"""
+node_count(t) = isterm(t) ? reduce(+, node_count(x) for x in  getargs(t), init=0) + 1 : 1
