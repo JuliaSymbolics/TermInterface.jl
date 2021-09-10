018fba63b4378a411605ec1d5e5b11deac3ab218
Alessandro <none>
2021-09-10 12:17:51 +0200

    docs

--- src/TermInterface.jl
+++ src/TermInterface.jl
@@ -97,12 +97,13 @@ end
 
 
 """
-    similarterm(x, head, args, symtype=nothing; metadata=nothing)
+    similarterm(x, head, args, symtype=nothing; metadata=nothing, exprhead=exprhead(x))
 
 Returns a term that is in the same closure of types as `typeof(x)`,
 with `head` as the head and `args` as the arguments, `type` as the symtype
 and `metadata` as the metadata. By default this will execute `head(args...)`.
-`x` parameter can also be a `Type`.
+`x` parameter can also be a `Type`. The `exprhead` keyword argument is useful 
+when manipulating `Expr`s.
 """
 similarterm(x, head, args, symtype=nothing; metadata=nothing, exprhead=exprhead(x)) = 
     similarterm(typeof(x), head, args, symtype; metadata=metadata, exprhead=exprhead)
