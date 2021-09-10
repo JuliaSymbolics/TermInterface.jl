f5c9e35b211e4832717644e2d2d3bf7075b93e1f
Alessandro <none>
2021-09-10 12:14:40 +0200

    basic tests

---
+++ test/runtests.jl
@@ -0,0 +1,19 @@
+using TermInterface
+using Test
+
+@testset "Expr" begin 
+    ex = :(f(a,b))
+    @test operation(ex) == :f
+    @test arguments(ex) == [:a, :b]
+    @test exprhead(ex) == :call
+    @test ex == similarterm(ex, :f, [:a, :b])
+
+    ex = :(arr[i, j])
+    @test operation(ex) == :ref
+    @test arguments(ex) == [:arr, :i, :j]
+    @test exprhead(ex) == :ref
+    @test ex == similarterm(Expr, :ref, [:arr, :i, :j]; exprhead=:ref)
+    @test ex == similarterm(ex, :ref, [:arr, :i, :j])
+end
+
+# TODO add SymbolicUtils tests??
