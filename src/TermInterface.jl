b915767f0eae82390362c98350eb61f801f467fe
Willow Ahrens <willow@csail.mit.edu>
2024-01-24 10:58:45 -0500

    fix

--- src/TermInterface.jl
+++ src/TermInterface.jl
@@ -96,9 +96,14 @@ end
 Returns a term that is in the same closure of types as `typeof(x)`,
 with `head` as the head and `args` as the arguments, `type` as the symtype
 and `metadata` as the metadata. By default this will execute `head(args...)`.
-`x` parameter can also be a `Type`.
+`x` parameter can also be a `Type`. Implementers should define similarterm on the
+type of `x` and not on `x` itself.
 """
 function similarterm(x, head, args, symtype = nothing; metadata = nothing)
+  similarterm(typeof(x), head, args, symtype = symtype, metadata = metadata)
+end
+
+function similarterm(x::DataType, head, args, symtype = nothing; metadata = nothing)
   head(args...)
 end
 
