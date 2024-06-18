125e6eacdf58781efd9f53f32a538473f1ef823b
Alessandro Cheli <a@a.a>
2024-06-18 22:01:34 +0200

    adjust interface

--- src/TermInterface.jl
+++ src/TermInterface.jl
@@ -69,20 +69,30 @@ export operation
 
 Returns the arguments to the function call in a function call expression.
 `iscall(x)` must be true as a precondition.
+
+Depending on the type and internal representation of `x`,
+`arguments(x)` may return an unsorted collection nondeterministically,
+This is to make sure to retrieve the arguments of an expression when the order of arguments does not matter 
+but the speed of the operation does.
+To ensure to retrieve arguments in a sorted manner, you can use 
+and implement the function `sorted_arguments`.
 """
 function arguments end
 export arguments
 
 """
-    unsorted_arguments(x::T)
+    sorted_arguments(x::T)
 
-If x is a expression satisfying `iscall(x)` and your expression type `T` provides
-and optimized implementation for storing the arguments, this function can 
-be used to retrieve the arguments when the order of arguments does not matter 
-but the speed of the operation does.
+Returns the arguments to the function call in a function call expression, in a **sorted fashion**.
+`iscall(x)` must be true as a precondition. Analogous to `arguments`, 
+but ensures that the operation is deterministic and always returns 
+the arguments in the order they are stored.
+
+By default, this redirects to `arguments`, therefore implementing 
+it is optional.
 """
-unsorted_arguments(x) = arguments(x)
-export unsorted_arguments
+sorted_arguments(x) = arguments(x)
+export sorted_arguments
 
 
 """
